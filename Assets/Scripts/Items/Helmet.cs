5771e1f4804f37cbcd69746f4a886d1183c2ad4b
Omer Cohen <omercohen213@gmail.com>
2022-07-27 21:19:53 +0300

    Inventory, enemy item drops. abilities

--- Assets/Scripts/Items/Helmet.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System;
-
-
-[CreateAssetMenu(fileName = "ScriptableObjects", menuName = "ScriptableObjects/Helmet")]
-public class Helmet : Item
-{
-    public int defense;
-}
